eaea16504c919ee4659f529c594db9e49ecaf052
xiaolingsundberg <>
2026-05-31 20:33:19 +0000

    Missing chapter

---
+++ section/missing_chapter/main.py
@@ -0,0 +1,9 @@
+print("Shopping Receipt")
+print("-----------------------")
+print("Item 1: $", 162*1.2)  # Price per weight
+print("Item 2: $", 25-3)  # Discounted price
+print("-----------------------")
+print("Subtotal: $", (162*1.2)+(25-3) )
+print("Tax (8%): $", ((162*1.2)+(25-3))*0.08)
+print("-----------------------")
+print("Thank you for shopping!")
