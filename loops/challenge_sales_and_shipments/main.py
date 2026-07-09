d12b71dc2e5f6fa1f0aa2cdf583714d001828f3a
xiaolingsundberg <>
2026-07-09 23:31:10 +0000

    Challenge: Sales and Shipments

---
+++ loops/challenge_sales_and_shipments/main.py
@@ -0,0 +1,13 @@
+# List of products with their initial stock levels at the start of the week
+products = [
+    ["Apples", 150],  
+    ["Bananas", 200],
+    ["Oranges", 100],
+    ["Mangoes", 120]
+]
+
+# List of products sold by the end of the week
+units_sold = [["Apples", 30], ["Bananas", 45], ["Oranges", 20], ["Mangoes", 10]]
+
+# New shipment received at the end of the week
+shipment_received = [["Apples", 50], ["Bananas", 70], ["Oranges", 30], ["Mangoes", 40]]
