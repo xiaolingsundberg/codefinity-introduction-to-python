e536e6f497ba9b842588fb25220a5dd58b618c83
xiaolingsundberg <>
2026-07-09 23:53:02 +0000

    Challenge: Sales and Shipments

--- loops/challenge_sales_and_shipments/main.py
+++ loops/challenge_sales_and_shipments/main.py
@@ -11,3 +11,8 @@ units_sold = [["Apples", 30], ["Bananas", 45], ["Oranges", 20], ["Mangoes", 10]]
 
 # New shipment received at the end of the week
 shipment_received = [["Apples", 50], ["Bananas", 70], ["Oranges", 30], ["Mangoes", 40]]
+
+for i in range(len(products)):
+    products[i][1] -= units_sold[i][1]
+    products[i][1] += shipment_received[i][1]
+    print()
